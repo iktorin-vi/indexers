f7166a32bc287deff8bb91eadcf0412f907f28d9
Viktoriia Kazniienko <nk.akk16@gmail.com>
2025-05-04 15:25:17 +0300

    add files

---
+++ Matrix.cs
@@ -0,0 +1,36 @@
+﻿namespace Class
+{
+    public class Matrix
+    {
+        private int[,] data;
+        public Matrix(int[,] arr)
+        {
+            data = arr;
+        }
+        public int this[int index]
+        {
+            get
+            {
+                int sum = 0;
+                for (int i = 0; i < data.GetLength(0); i++)
+                {
+                    sum += data[i, index];
+                }
+                return sum;
+            }
+        }
+        public double Rmeansquare
+        {
+            get
+            {
+                double sum = 0;
+                int n = data.GetLength(0);
+                int m = data.GetLength(1);
+                for (int i = 0; i < n; i++)
+                    for (int j = 0; j < m; j++)
+                        sum += data[i, j] * data[i, j];
+                return Math.Sqrt(sum / (n * m));
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿//Done: Viktoria Kaznienko 
+//Group:3
+//Variant:7
+////////////
+
+using System;
+using Class;
+class Program
+{
+    static void Main()
+    {
+        int[,] arr = {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 }
+            };
+        Matrix m = new Matrix(arr);
+        Console.WriteLine("Sum of values of the third column:" + m[2]);
+        Console.WriteLine("Root mean square: " + m.Rmeansquare);
+    }
+
+}
